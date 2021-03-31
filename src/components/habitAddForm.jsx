41eaba5e064e8c6069754d4db8ca69c4c195b4f4
seanwuvely <seanwuvely@gmail.com>
2021-03-31 15:11:46 +0900

    add habit-tracker

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,29 @@
+import React, { memo } from "react";
+
+const HabitAddForm = memo((props) => {
+  const inputRef = React.createRef();
+  const formRef = React.createRef();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const name = inputRef.current.value;
+    name && props.onAdd(name);
+    // this.inputRef.current.value = "";
+    formRef.current.reset();
+    inputRef.current.focus();
+  };
+
+  return (
+    <form ref={formRef} className="add-form" onSubmit={onSubmit}>
+      <input
+        type="text"
+        className="add-input"
+        placeholder="Habit"
+        ref={inputRef}
+      />
+      <button className="add-button">Add</button>
+    </form>
+  );
+});
+
+export default HabitAddForm;
